bed7a50a48719da6030bc5d4e957cd455c0e2801
Y-takumi0 <134852542+Y-takumi0@users.noreply.github.com>
2023-07-16 12:40:57 +0000

    [start]5-27[07/16]

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
-  def edit
-    @user = User.find(params[:id])
+def edit
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to users_path
   end
+    @user = User.find(params[:id])
+end
 
   def index
+    @users = User.all
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
