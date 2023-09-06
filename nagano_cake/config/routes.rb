98954c202a28a9b69c8fa2564e07cda3456c4af6
Y-takumi0 <134852542+Y-takumi0@users.noreply.github.com>
2023-09-06 01:35:29 +0000

    apply4-2

--- nagano_cake/config/routes.rb
+++ nagano_cake/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :items, only: [:index, :show]
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
@@ -7,5 +11,25 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ namespace :admin do
+   root to: 'homes#top'
+   resources :items, only: [:index, :new, :create, :show, :edit, :update]
+   resources :genres, only: [:index, :create, :edit, :update]
+   resources :customers, only: [:index, :show, :edit, :update]
+   resources :orders, only: [:show, :update]
+   resources :order_details, only: [:update]
+ end
+  scope module: :public do
+    get "customers/mypage" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    patch "customers/information" => "customers#update"
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete "cart_items/destroy" => "cart_items#destroy_all"
+    resources :orders, only: [:index, :new, :create, :show]
+    get "orders/complete" => "orders#complete"
+    post "orders/confirmation" => "orders#confirmation"
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
 end
