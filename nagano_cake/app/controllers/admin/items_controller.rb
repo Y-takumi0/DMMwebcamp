9c8b955f3ee0505a5e1d8183b9db145f2da19208
Y-takumi0 <134852542+Y-takumi0@users.noreply.github.com>
2023-09-07 09:00:30 +0000

    aplle5

--- nagano_cake/app/controllers/admin/items_controller.rb
+++ nagano_cake/app/controllers/admin/items_controller.rb
@@ -1,7 +1,25 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
 
   def show
     @item = Item.find(params[:id])
   end
 
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :status, :image)
+  end
 end
--- nagano_cake/app/controllers/admin/sessions_controller.rb
+++ nagano_cake/app/controllers/admin/sessions_controller.rb
@@ -20,7 +20,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   protected
     def after_sign_in_path_for(resource)
-      new_admin_session_path
+      admin_customers_path
     end
 
     def after_sign_out_path_for(resource)
